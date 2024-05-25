3ccf1104040b7ec6f05f408181443aab91c2a509
ADEYEMI NURUDEEN OLATUNBOSUN <adeyemisworld@gmail.com>
2024-05-25 12:27:52 +0100

    Add project files.

---
+++ Services/Interfaces/IApartmentService.cs
@@ -0,0 +1,18 @@
+﻿
+using BookingManager.Models;
+
+namespace BookingManager.Services.Interfaces
+{
+    public interface IApartmentService
+    {
+        //GetApartment
+        ApartmentDTO GetApartment(int id);
+        IList<ApartmentDTO> GetApartments();
+
+        //AddApartment
+        BaseResponse AddApartment(ApartmentDTO request);
+
+        //UpdateApartment
+        BaseResponse UpdateApartment(int id, ApartmentDTO request);
+    }
+}
